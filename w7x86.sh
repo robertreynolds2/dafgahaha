e7bee9ce621680c712e2ead019afd3859bb1530d
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-18 19:35:41 +0700

    Create w7x86.sh

---
+++ w7x86.sh
@@ -0,0 +1,33 @@
+echo "===================================="
+echo "Download windows files"
+echo "===================================="
+curl -L -o w7x86.img https://bit.ly/akuhnetw7x86
+echo "===================================="
+echo "Download ngrok"
+echo "===================================="
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+./ngrok tcp 3388 &>/dev/null &
+echo "===================================="
+echo Downloading File From akuh.net
+echo "===================================="
+apt-get install qemu > /dev/null 2>&1
+echo "===================================="
+echo "Wait"
+echo "Starting Windows"
+echo "===================================="
+echo RDP Address:
+echo "===================================="
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Ctrl+C To Copy"
+echo "Wait 1-2 minute to finish bot"
+echo "Dont Close This Tab"
+echo "Please support akuh.net thank you"
+echo "===================================="
+echo "===================================="
